e2f86aac9ea486fdbbacb2c23cd612fb15ba055f
krlslman <selmankorall@gmail.com>
2022-07-23 11:52:38 +0300

    createSlice metodu ile redux state tanımlandı

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,17 +4,32 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
 
 export default function Navbar() {
+  const navigate = useNavigate();
+  const user = true;
+
+  const handleLogout = () => {
+    //* clear user data and go to login page
+    navigate('/')
+  }
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" color="secondary">
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 , cursor:"pointer" }} onClick={()=> navigate("/")}>
             Clarusway News
           </Typography>
 
-          <Button color="inherit">Login</Button>
+        {user ? (
+          <Button color="inherit" onClick={handleLogout}>Logout</Button>
+          ) : (
+            <Button color="inherit">Login</Button>
+        ) }
+
+          <Button color="inherit" onClick={()=>navigate('/')}>Login</Button>
         </Toolbar>
       </AppBar>
     </Box>
---
+++ src/features/authSlice.jsx
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+	user: '',
+};
+
+const authSlice = createSlice({
+	name: "auth",
+	initialState,
+	reducers:{
+		setUser: (state, action) => {
+			state.user = action.payload
+		},
+		clearUser: (state, action)=> {
+			state.user = '';
+		}
+	}
+})
